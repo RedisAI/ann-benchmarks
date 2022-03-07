6ae9bff1cb04f2b3d340f6671c3ae4c4c0076fb3
filipecosta90 <filipecosta.90@gmail.com>
2022-03-07 00:40:31 +0000

    Changes towards redisbench_admin integration. Workdir fix

--- multirun.py
+++ multirun.py
@@ -2,10 +2,12 @@ from os import system, path, makedirs
 from multiprocessing import Process
 import argparse
 import time
+import json
 from redis import Redis
 from redis.cluster import RedisCluster
 import h5py
-from ann_benchmarks.main import positive_int
+import os
+import pathlib
 from ann_benchmarks.results import get_result_filename
 
 if __name__ == "__main__":
@@ -17,9 +19,14 @@ if __name__ == "__main__":
         help='the dataset to load training points from',
         default='glove-100-angular')
     parser.add_argument(
+        '--json-output',
+        help='Path to the output file. If defined will store the results in json format.',
+        default=""
+    )
+    parser.add_argument(
         "-k", "--count",
-        default=10,
-        type=positive_int,
+        default="10",
+        type=str,
         help="the number of near neighbours to search for")
     parser.add_argument(
         '--host',
@@ -27,7 +34,7 @@ if __name__ == "__main__":
         default=None)
     parser.add_argument(
         '--port',
-        type=positive_int,
+        type=str,
         help='the port "host" is listening on',
         default=None)
     parser.add_argument(
@@ -42,16 +49,16 @@ if __name__ == "__main__":
         default=None)
     parser.add_argument(
         '--build-clients',
-        type=int,
+        type=str,
         metavar='NUM',
         help='total number of clients running in parallel to build the index (could be 0)',
-        default=1)
+        default="1")
     parser.add_argument(
         '--test-clients',
-        type=int,
+        type=str,
         metavar='NUM',
         help='total number of clients running in parallel to test the index (could be 0)',
-        default=1)
+        default="1")
     parser.add_argument(
         '--force',
         help='re-run algorithms even if their results already exist',
@@ -67,6 +74,12 @@ if __name__ == "__main__":
         help='run only the named run group',
         default=None)
     parser.add_argument(
+        '--runs',
+        type=str,
+        help='run each algorithm instance %(metavar)s times and use only'
+             ' the best result',
+        default="3")
+    parser.add_argument(
         '--cluster',
         action='store_true',
         help='working with a cluster')
@@ -76,10 +89,11 @@ if __name__ == "__main__":
 
     if isredis:
         redis = RedisCluster if args.cluster else Redis
-        redis = redis(host=args.host, port=args.port, password=args.auth, username=args.user)
+        redis = redis(host=args.host, port=int(args.port), password=args.auth, username=args.user)
 
-    base = 'python run.py --local --algorithm ' + args.algorithm + ' -k ' + str(args.count) + \
-           ' --dataset ' + args.dataset
+
+    base = 'python3 run.py --local --algorithm ' + args.algorithm + ' -k ' + args.count + \
+           ' --dataset ' + args.dataset + " --runs {} ".format(args.runs)
 
     if args.host:       base += ' --host ' + str(args.host)
     if args.port:       base += ' --port ' + str(args.port)
@@ -91,9 +105,12 @@ if __name__ == "__main__":
 
     base_build = base + ' --build-only --total-clients ' + str(args.build_clients)
     base_test = base + ' --test-only --runs 1 --total-clients ' + str(args.test_clients)
-
-    if args.build_clients > 0:
-        clients = [Process(target=system, args=(base_build + ' --client-id ' + str(i),)) for i in range(1, args.build_clients + 1)]
+    workdir = pathlib.Path(__file__).parent.absolute()
+    print("Changing the workdir to {}".format(workdir))
+    os.chdir(workdir)
+    results_dict = {}
+    if int(args.build_clients) > 0:
+        clients = [Process(target=system, args=(base_build + ' --client-id ' + str(i),)) for i in range(1, int(args.build_clients) + 1)]
 
         t0 = time.time()
         for client in clients: client.start()
@@ -102,23 +119,36 @@ if __name__ == "__main__":
         print(f'total build time: {total_time}\n\n')
 
         fn = get_result_filename(args.dataset, args.count)
+        print(fn)
         fn = path.join(fn, args.algorithm)
         if not path.isdir(fn):
             makedirs(fn)
         fn = path.join(fn, 'build_stats.hdf5')
         f = h5py.File(fn, 'w')
         f.attrs["build_time"] = total_time
+        print(fn)
+        index_size = -1
         if isredis:
             if args.cluster:
-                f.attrs["index_size"] = -1 # TODO: get total size from all the shards
+                index_size = -1 # TODO: get total size from all the shards
             else:
-                f.attrs["index_size"] = redis.ft('ann_benchmark').info()['vector_index_sz_mb']*0x100000
+                index_size = redis.ft('ann_benchmark').info()['vector_index_sz_mb']
+            f.attrs["index_size"] = index_size
         f.close()
+        results_dict["build"] = {"total_clients":args.build_clients, "build_time": total_time, "vector_index_sz_mb": index_size }
 
-    if args.test_clients > 0:
+
+
+    if int(args.test_clients) > 0:
         queriers = [Process(target=system, args=(base_test + ' --client-id ' + str(i),)) for i in range(1, args.test_clients + 1)]
         t0 = time.time()
         for querier in queriers: querier.start()
         for querier in queriers: querier.join()
         query_time = time.time() - t0
         print(f'total test time: {query_time}')
+        results_dict["query"] = {"total_clients":args.test_clients, "test_time": query_time }
+
+    if args.json_output != "":
+        with open(args.json_output,"w")as json_out_file:
+            print(f'storing json result into: {args.json_output}')
+            json.dump(results_dict,json_out_file)
