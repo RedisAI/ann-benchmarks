24f564bb752d61b89760ac91fd7f204b96de4b23
filipecosta90 <filipecosta.90@gmail.com>
2022-07-18 16:08:21 +0100

    Updating elastic client: WIP

--- ann_benchmarks/algorithms/elasticsearch.py
+++ ann_benchmarks/algorithms/elasticsearch.py
@@ -148,7 +148,7 @@ class ElasticsearchScriptScoreQuery(BaseANN):
 
     def check_index_does_not_exist(self):
         print("Checking if index named {} exists.".format(self.index))
-        res = self.es.indices.get_alias(name=self.index)
+        res = self.es.indices.get_alias(index=self.index)
         print("Indices: {}".format(res))
         if self.index in res.keys():
             print("Detected index. deleting it...")
