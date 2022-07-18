71a056717704ac901df39d7ce793dcc268e75193
filipecosta90 <filipecosta.90@gmail.com>
2022-07-18 16:30:53 +0100

    Updating elastic client: WIP

--- ann_benchmarks/algorithms/elasticsearch.py
+++ ann_benchmarks/algorithms/elasticsearch.py
@@ -119,7 +119,7 @@ class ElasticsearchScriptScoreQuery(BaseANN):
         print("\tIndex properties {}".format(mappings))
         try:
             self.es.indices.create(index=self.index, mappings=mappings,
-                                   settings=dict(number_of_shards=1, number_of_replicas=0), timeout=f'{self.timeout}m')
+                                   settings=dict(number_of_shards=1, number_of_replicas=0), timeout=f'{self.timeout}m', max_retries=10)
         except ConnectionTimeout as e:
             if not self.wait_for_readiness():
                 raise e
