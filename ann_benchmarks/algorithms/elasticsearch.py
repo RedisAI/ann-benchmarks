3e4200772e41212ae39bfd68228c8ce2480f1324
filipecosta90 <filipecosta.90@gmail.com>
2022-07-18 15:44:39 +0100

    Updating elastic client: WIP

--- ann_benchmarks/algorithms/elasticsearch.py
+++ ann_benchmarks/algorithms/elasticsearch.py
@@ -18,6 +18,7 @@ from ann_benchmarks.algorithms.base import BaseANN
 # By default, it writes an INFO statement for every request.
 logging.getLogger("elasticsearch").setLevel(logging.WARN)
 
+
 # Uncomment these lines if you want to see timing for every HTTP request and its duration.
 # logging.basicConfig(level=logging.INFO)
 # logging.getLogger("elasticsearch").setLevel(logging.INFO)
@@ -27,7 +28,7 @@ def es_wait(es):
     for i in range(30):
         try:
             res = es.cluster.health(wait_for_status='yellow', timeout='1s')
-            if not res['timed_out']: # then status is OK
+            if not res['timed_out']:  # then status is OK
                 print("Elasticsearch is ready")
                 return
         except URLError:
@@ -54,24 +55,47 @@ class ElasticsearchScriptScoreQuery(BaseANN):
         u = conn_params['user'] if conn_params['user'] is not None else 'elastic'
         a = conn_params['auth'] if conn_params['auth'] is not None else ''
         self.index = "ann_benchmark"
-        try:
-            self.es = Elasticsearch(f"http://{h}:{p}",  request_timeout=self.timeout, basic_auth=(u, a), ca_certs=environ.get('ELASTIC_CA', DEFAULT))
-        except Exception:
-            self.es = Elasticsearch(f"https://{h}:{p}", request_timeout=self.timeout, basic_auth=(u, a), ca_certs=environ.get('ELASTIC_CA', DEFAULT))
+        self.es = Elasticsearch(f"{h}:{p}", request_timeout=self.timeout, basic_auth=(u, a),
+                                ca_certs=environ.get('ELASTIC_CA', DEFAULT))
         self.batch_res = []
         es_wait(self.es)
+        self.check_index_does_not_exist()
+        self.create_index()
+
+    def wait_for_readiness(self):
+        ready = False
+        for i in range(self.timeout):
+            stats = self.es.indices.stats(index=self.index)
+            if stats['_shards']['total'] == stats['_shards']['successful']:
+                ready = True
+                break
+            sleep(1)
+        return ready
+
+    """
+    Based uppon https://www.elastic.co/guide/en/elasticsearch/reference/current/tune-for-indexing-speed.html
+    and given we're doing bulk index operations and only after fully indexing we do the querying we should tune
+    the index refresh interval to larger timeframes ( default is 1s ) or even disable it while ingesting.
+    """
+
+    def update_refresh_interval(self, new_refresh_interval):
+        previous_settings = self.es.indices.get_settings(index=self.index)
+        current_refresh_interval = previous_settings[self.index]['settings']['index']['refresh_interval']
+        print("Altering index refresh interval from {} to {}".format(
+            new_refresh_interval, current_refresh_interval))
+        self.es.indices.put_settings(index=self.index, settings={
+            "index.refresh_interval": new_refresh_interval
+        })
 
     def fit(self, X):
-        def wait_for_readiness():
-            ready = False
-            for i in range(self.timeout):
-                stats = self.es.indices.stats(index=self.index)
-                if stats['_shards']['total'] == stats['_shards']['successful']:
-                    ready = True
-                    break
-                sleep(1)
-            return ready
-    
+        def gen():
+            for i, vec in enumerate(X):
+                yield {"_op_type": "index", "_index": self.index, "vec": vec.tolist(), 'id': str(i)}
+
+        (_, errors) = bulk(self.es, gen(), chunk_size=500, max_retries=9)
+        assert len(errors) == 0, errors
+
+    def create_index(self):
         mappings = dict(
             properties=dict(
                 id=dict(type="keyword", store=True),
@@ -84,36 +108,26 @@ class ElasticsearchScriptScoreQuery(BaseANN):
                 )
             )
         )
+        print("Creating elastic index named {}".format(self.index))
+        print("\t\tIndex properties {}".format(mappings))
         try:
-            self.es.indices.create(index=self.index, mappings=mappings, settings=dict(number_of_shards=1, number_of_replicas=0), timeout=f'{self.timeout}m')
+            self.es.indices.create(index=self.index, mappings=mappings,
+                                   settings=dict(number_of_shards=1, number_of_replicas=0), timeout=f'{self.timeout}m')
         except ConnectionTimeout as e:
-            if not wait_for_readiness():
+            if not self.wait_for_readiness():
                 raise e
         except BadRequestError as e:
-            if 'resource_already_exists_exception' not in e.message: raise e
-
-        # self.es.indices.put_mapping(properties=properties, index=self.index)
-
-        def gen():
-            for i, vec in enumerate(X):
-                yield { "_op_type": "index", "_index": self.index, "vec": vec.tolist(), 'id': str(i) }
-
-        (_, errors) = bulk(self.es, gen(), chunk_size=500, max_retries=9)
-        assert len(errors) == 0, errors
-
-        try:
-            self.es.indices.refresh(index=self.index)
-        except ConnectionTimeout as e:
-            if not wait_for_readiness():
+            if 'resource_already_exists_exception' not in e.message:
                 raise e
-            self.es.indices.refresh(index=self.index)
-        self.es.indices.forcemerge(index=self.index, max_num_segments=1)
+        print("Setting refresh interval to 60 seconds")
+        self.update_refresh_interval(60)
 
     def set_query_arguments(self, ef):
         self.ef = ef
 
     def query(self, q, n):
-        knn = dict(field='vec', query_vector=q.tolist(), k=n, num_candidates=self.ef)
+        knn = dict(field='vec', query_vector=q.tolist(),
+                   k=n, num_candidates=self.ef)
         res = self.es.knn_search(index=self.index, knn=knn, source=False, docvalue_fields=['id'],
                                  stored_fields="_none_", filter_path=["hits.hits.fields.id"])
         return [int(h['fields']['id'][0]) for h in res['hits']['hits']]
@@ -124,3 +138,14 @@ class ElasticsearchScriptScoreQuery(BaseANN):
     def get_batch_results(self):
         return self.batch_res
 
+    def freeIndex(self):
+        print("Deleting elastic index named {}".format(self.index))
+        self.es.indices.delete(index=self.index)
+
+    def check_index_does_not_exist(self):
+        print("Checking if index named {} exists.".format(self.index))
+        if self.index in self.es.indices.get_alias("*"):
+            print("Detected index. deleting it...")
+            self.freeIndex()
+        else:
+            print("No index detected.")
